f80e7fccce822273d51f46eefc5ea1fbd740d701
srmzhk17 <srmzhk17@gmail.com>
2024-10-02 00:32:17 +0300

    Let's get started!

---
+++ src/main/java/org/srmzhk/cryptobot/CryptoBotApplication.java
@@ -0,0 +1,13 @@
+package org.srmzhk.cryptobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/srmzhk/cryptobot/CryptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.srmzhk.cryptobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
